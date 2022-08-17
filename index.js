0018d9fef8462621e3fda9d80791c903c1f5fda7
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-08-17 22:13:24 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const URL = "https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3";
+const HTML_SELECTOR = ".plainlist ul li";
+
+axios
+  .get(URL)
+  .then(response => {
+    const $ = cheerio.load(response.data);
+    const data = $(HTML_SELECTOR);
+    
+    const countries = [];
+    data.each((idx, element) => {
+      countries.push({
+        country: $(element).children("a").text(),
+        iso3: $(element).children("span").text(),
+      });
+    });
+    fs.writeFileSync("./countries.json", JSON.stringify(countries));
+    console.log(countries);
+  })
